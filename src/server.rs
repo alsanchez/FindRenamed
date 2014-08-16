9e1c8009c3c3911de176c0309b70381c9d5f6f80
Alejandro Sánchez <contacto@alsanchez.es>
2014-08-16 10:58:43 +0200

    Replace TCP server in favour of stdin / stdout

--- src/server.rs
+++ src/server.rs
@@ -1,50 +1,53 @@
-use std::io::{Listener, Acceptor};
+use std::io::stdio;
 use std::collections::HashMap;
 use std::str;
 use std::vec::Vec;
-use std::io::net::tcp::TcpListener;
 use util;
 
 pub fn start_server() {
 
-    let mut acceptor = TcpListener::bind("127.0.0.1", 9876).listen().unwrap();
-    println!("listening started, ready to accept");
-    let opt_stream = acceptor.accept();
+    manage_client(&mut stdio::stdin_raw(), &mut stdio::stdout_raw());
+}
+
+fn manage_client(input: &mut Reader, output: &mut Writer) {
+
     let mut buffer = [0u8, ..4096];
-    let mut stream = opt_stream.unwrap();
+
     loop {
-        let read_bytes = stream.read(buffer).unwrap();
+        let read_bytes = input.read(buffer).unwrap();
         let command = str::from_utf8(buffer.slice(0, read_bytes)).unwrap().trim();
 
         if command.starts_with("checksum") {
             let file_path = command.replace("checksum ", "");
-            stream.write(b"OK ");
-            stream.write(util::get_file_checksum(&Path::new(file_path)).as_bytes());
-            stream.write(b"\n");
+            output.write(b"OK ");
+            output.write(util::get_file_checksum(&Path::new(file_path)).as_bytes());
+            output.write(b"\n");
         } else if command.starts_with("metadata") {
             let dir_path = command.replace("metadata ", "");
             let mut file_map = HashMap::<(u64, u64), Vec<String>>::new();
             util::examine_files(&Path::new(dir_path), &mut file_map);
+            output.write(b"OK START\n");
             for (key, values) in file_map.iter() {
                 for value in values.iter() {
                     let &(size, mod_date) = key;
-                    stream.write(size.to_string().as_bytes());
-                    stream.write(b" ");
-                    stream.write(mod_date.to_string().as_bytes());
-                    stream.write(b" ");
-                    stream.write(value.as_bytes());
-                    stream.write_u8(0u8);
-                    stream.write(b"\n");
+                    output.write(size.to_string().as_bytes());
+                    output.write(b" ");
+                    output.write(mod_date.to_string().as_bytes());
+                    output.write(b" ");
+                    output.write(value.as_bytes());
+                    output.write_u8(0u8);
+                    output.write(b"\n");
                 }
             }
+            output.write(b"END\n");
         }
         else if command == "exit" {
             break;
         } else {
-            stream.write(b"ERR Invalid command \"");
-            stream.write(command.as_bytes());
-            stream.write(b"\"\n");
+            output.write(b"ERR Invalid command \"");
+            output.write(command.as_bytes());
+            output.write(b"\"\n");
         }
     }
-}
 
+}
